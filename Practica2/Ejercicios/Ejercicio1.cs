b347b6ddcaaa4a6960e0082f5ad2b6e24f3304ed
AyrtonRada <axel582011@hotmail.com>
2023-03-08 16:56:58 -0300

    Ejercicio 1

---
+++ Practica2/Ejercicios/Ejercicio1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica2.Ejercicios
+{
+    public class Ejercicio1
+    {
+           public static void Ejercicio1DividirCero()
+        {
+            try
+            {
+                Console.WriteLine("Ejercicio 1. \nIngrese un número: ");
+                int numero = Int32.Parse(Console.ReadLine());
+                int resultado = numero / 0;
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (OverflowException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("La división terminó.");
+            }
+            
+        }
+    }
+}
---
+++ Practica2/Program.cs
@@ -0,0 +1,18 @@
+﻿using Practica2.Ejercicios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ejercicio1.Ejercicio1DividirCero();
+            Console.ReadKey();
+        }
+    }
+}
