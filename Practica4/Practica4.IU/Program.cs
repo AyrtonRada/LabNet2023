0dd85ed7636ec127fd29d343ea4104cd59be4624
AyrtonRada <axel582011@hotmail.com>
2023-03-15 19:21:55 -0300

    consulta simple entidad Empleados y Clientes

--- Practica4/Practica4.IU/Program.cs
+++ Practica4/Practica4.IU/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Practica4.Entities;
+using Practica4.Logic;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,7 +12,27 @@ namespace Practica4.IU
     {
         static void Main(string[] args)
         {
+            // Mostrar la lista de empleados
+            EmployeesLogic employeesLogic = new EmployeesLogic();
 
+            foreach (Employees employee in employeesLogic.GetAll())
+            {
+                Console.WriteLine($"{employee.FirstName} - {employee.LastName}");
+            }
+
+            Console.ReadKey();
+
+
+            // Mostrar la lista de clientes
+            CustomersLogic customersLogic = new CustomersLogic();
+
+            foreach (Customers customers in customersLogic.GetAll())
+            {
+                Console.WriteLine($"{customers.ContactName} - {customers.CompanyName}");
+            }
+
+            Console.ReadKey();
         }
     }
+
 }
---
+++ Practica4/Practica4.Logic/CustomersLogic.cs
@@ -0,0 +1,18 @@
+﻿using Practica4.Data;
+using Practica4.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica4.Logic
+{
+    public class CustomersLogic : BaseLogic
+    {        
+        public List<Customers> GetAll()
+        {
+            return context.Customers.ToList();
+        }
+    }
+}
