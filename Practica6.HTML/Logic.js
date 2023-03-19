ac9038b809e1750212fb814f533cf6cceee7ade5
AyrtonRada <axel582011@hotmail.com>
2023-03-19 20:07:14 -0300

    logica de verificacion

--- Practica6.HTML/Logic.js
+++ Practica6.HTML/Logic.js
@@ -1,6 +1,35 @@
-window.addEventListener("load", ()=>{
+window.addEventListener('load', ()=>{
 
+    //Selectores de clases
+    let entry = document.querySelector('.entry');
+    let clue = document.querySelector('.clue');
+    let tryButton = document.querySelector('.try-button');
     
+    /**
+    * LOGICA
+    */
+
+    //Numero random
+    let numRand = () => Math.floor(Math.random() * Math.floor(20)) +1;
+    let numero = numRand()
+    
+    //Comprobar si acertó
+    
+    tryButton.addEventListener('click', ()=> {
+        
+        if(entry.value == numero){
+            clue.textContent = 'Acertaste!';
+        } else if( entry.value > numero){
+            clue.textContent = 'Muy Alto!';
+        } else{
+            clue.textContent = 'Muy Bajo';
+        }
+
+    })
+
+
+
+
 
 
 
