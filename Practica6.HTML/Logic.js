2c0331b11773161bf1aa50f628e0cb00e2b24498
AyrtonRada <axel582011@hotmail.com>
2023-03-19 22:14:40 -0300

    Logica de highscore y boton de reinicio

--- Practica6.HTML/Logic.js
+++ Practica6.HTML/Logic.js
@@ -6,7 +6,7 @@ window.addEventListener('load', ()=>{
     let tryButton = document.querySelector('.try-button');
     let unknown = document.querySelector('.unknown');
     let scoreValue = document.querySelector('.score-value');
-    let highscore = document.querySelector('.highscore-value');
+    let highscoreValue = document.querySelector('.highscore-value');
     let reinicio = document.querySelector('.reset-button');
 
     /**
@@ -14,41 +14,59 @@ window.addEventListener('load', ()=>{
     */
 
     //Valor de las vidas
-    let score = 20
+    let score = 20;
+    let array = [];
+    let cont = 0;
 
     //Numero random
     let numRand = () => Math.floor(Math.random() * Math.floor(20)) +1;
-    let numero = numRand()
+    let numero = numRand();
     
     //Comprobar si acertó y Actualizar el score
     
     tryButton.addEventListener('click', ()=> {
         
         if(entry.value == numero){
-            clue.textContent = 'Acertaste!'
-            unknown.textContent= numero
-            scoreValue.textContent = score
-            highscore.textContent = score
-            
-
+            clue.textContent = 'Acertaste!';
+            unknown.textContent= numero;
+            scoreValue.textContent = score;            
+            array.push(score)
+            highscore()
+             
         } else if( entry.value > numero){
             clue.textContent = 'Muy Alto!';
-            unknown.textContent= '?'
-            scoreValue.textContent= (score-=1)
+            unknown.textContent= '?';
+            scoreValue.textContent= (score-=1);
         } else{
             clue.textContent = 'Muy Bajo';
-            unknown.textContent= '?'
-            scoreValue.textContent= (score-=1)
+            unknown.textContent= '?';
+            scoreValue.textContent= (score-=1);
         }
         
     })
+
+    //HighScore
+
+    function highscore(){
+        
+        for (let i = 0; i < array.length; i++) {
+            
+            if(cont < array[i]){
+                cont = array[i]
+                highscoreValue.textContent = score  
+            }
+        }
+    }
 
     //Reiniciar el juego
 
     reinicio.addEventListener('click', ()=> {
 
-        
-
+        numero = numRand();
+        score = 20;
+        scoreValue.textContent = 20;
+        clue.textContent = 'Intentalo!';
+        entry.value = '';        
     })
 
 
