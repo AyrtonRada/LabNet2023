2742d5204eea8cc17afc6105092b7f516768c51a
AyrtonRada <axel582011@hotmail.com>
2023-03-27 20:48:38 -0300

    API Empleados. Sustitucion del ValuesController por EmployeesController Creacion CRUD.

---
+++ Practica4/Practica4/Controllers/EmployeesController.cs
@@ -0,0 +1,127 @@
+﻿using Practica4.Entities;
+using Practica4.Logic;
+using Practica4.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Practica4.Controllers
+{
+    
+    [RoutePrefix("api/employees")]
+    public class EmployeesController : ApiController
+    {
+        EmployeesLogic logic = new EmployeesLogic();
+
+        // GET api/employees
+        [HttpGet]        
+        public IHttpActionResult Get()
+        {
+
+            try
+            {
+                List<Employees> empleados = logic.GetAll();
+                List<EmployeesViewApi> empleadosViews = empleados.Select(x => new EmployeesViewApi
+                {
+                    EmployeeID = x.EmployeeID,
+                    FirstName = x.FirstName,
+                    LastName = x.LastName
+                }).ToList();
+                return Ok(empleadosViews);
+
+            }
+            catch (Exception)
+            {
+                return InternalServerError();
+            }
+        }
+
+        // GET api/employees/5
+        [HttpGet]        
+        public IHttpActionResult Get(int id)
+        {
+            try
+            {
+                Employees employees = logic.GetOne(id);
+                var result = new EmployeesViewApi
+                {
+                    EmployeeID = employees.EmployeeID,
+                    FirstName = employees.FirstName,
+                    LastName = employees.LastName
+                };
+                return Ok(result);
+            }
+            catch (Exception)
+            {
+                return BadRequest();
+            }
+        }
+
+        // POST api/employees
+        [HttpPost]
+        public IHttpActionResult Post([FromBody] EmployeesViewApi employeesView)
+        {
+            try
+            {
+                var employeesEntity = new Employees
+                {
+                    FirstName = employeesView.FirstName,
+                    LastName = employeesView.LastName
+                };
+                logic.Add(employeesEntity);
+                return Ok("Creado Exito!");
+            }
+            catch (Exception)
+            {
+                return BadRequest("Error al Crear Empleado");
+            }
+        }
+
+        [HttpPut]
+        // PUT api/employees/5
+        public IHttpActionResult Put(int id, [FromBody] EmployeesViewApi employeesView)
+        {
+            try
+            {
+                Employees empleados = logic.GetOne(id);
+                if(empleados != null && ModelState.IsValid)
+                {
+                    Employees employee = new Employees()
+                    {          
+                        EmployeeID = employeesView.EmployeeID,
+                        FirstName = employeesView.FirstName,
+                        LastName = employeesView.LastName
+                    };
+                    logic.Update(employee);
+                }
+                else
+                {
+                    return BadRequest("Datos inválidos");
+                }
+                return Ok("Empleado Actualizado");
+            }
+            catch 
+            {
+                return BadRequest("Error, No se Actualizó");
+            }
+        }
+
+        [HttpDelete]
+        // DELETE api/employees/5
+        public IHttpActionResult Delete(int id)
+        {
+            try
+            {
+                logic.Delete(id);
+                return Ok("Eliminación Exitosa!");
+            }
+            catch (Exception)
+            {
+                return BadRequest("Falló Eliminación");
+            }
+        }
+    }
+}
---
+++ Practica4/Practica4/Models/EmployeesViewApi.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Practica4.Models
+{
+    public class EmployeesViewApi
+    {
+        [Key]
+        public int EmployeeID { get; set; }
+
+        [Required]
+        [StringLength(20)]
+        public string LastName { get; set; }
+
+        [Required]
+        [StringLength(10)]
+        public string FirstName { get; set; }
+    }
+}
