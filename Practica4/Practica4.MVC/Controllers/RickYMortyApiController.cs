607f4f17941372263efe443205470d41352f61a5
AyrtonRada <axel582011@hotmail.com>
2023-03-27 15:50:58 -0300

    Consumo de API publica

---
+++ Practica4/Practica4.MVC/Controllers/RickYMortyApiController.cs
@@ -0,0 +1,43 @@
+﻿using Newtonsoft.Json;
+using Practica4.MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Practica4.MVC.Controllers
+{
+    public class RickYMortyApiController : Controller
+    {
+        // GET: RickYMortyApi
+        public async Task<ActionResult> Index()
+        {
+            HttpClient httpClient = new HttpClient();
+            string url = "https://rickandmortyapi.com/api/character";
+            var json = await httpClient.GetStringAsync(url);
+                                        
+            dynamic listApi = JsonConvert.DeserializeObject(json);
+
+            var seccionResult = listApi.results;
+
+            List<RickYMortyApiView> listRickMortyView = new List<RickYMortyApiView>();
+
+            foreach (var item in seccionResult)
+            {
+                RickYMortyApiView rickMortyApi = new RickYMortyApiView()
+                {
+                    Name = item.name,
+                    Status = item.status,
+                    Species = item.species,
+                    Gender = item.gender                    
+                };
+                listRickMortyView.Add(rickMortyApi);
+            }
+            return View(listRickMortyView);         
+            
+        }
+    }
+}
---
+++ Practica4/Practica4.MVC/Models/RickYMortyApiView.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Practica4.MVC.Models
+{
+    public class RickYMortyApiView
+    {
+        public string Name { get; set; }
+        public string Status { get; set; }
+        public string Species { get; set; }
+        public string Gender { get; set; }    
+    }
+
+    
+}
