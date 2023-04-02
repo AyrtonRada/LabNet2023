446272355ac1cf481cce0e6f03886d142ce4aa35
AyrtonRada <axel582011@hotmail.com>
2023-04-02 03:12:19 -0300

    Base html y componente Empleados

---
+++ Practica8.Angular/src/app/abmEmployees/abmEmployees.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { EmployeesComponent } from './Components/employees/employees.component';
+
+
+
+@NgModule({
+  declarations: [
+    EmployeesComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class AbmEmployeesModule { }
